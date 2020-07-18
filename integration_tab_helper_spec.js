83ded2dc97de0731f25c99dbb0e6a57efd349b1b
Jake Downs <mail@jakedowns.com>
2020-07-18 13:52:24 -0700

    slightly more generalized, removed unnecessary .then()'s

--- integration_tab_helper_spec.js
+++ integration_tab_helper_spec.js
@@ -1,63 +1,74 @@
-import {CONSTS} from '../support/constants'
-let {AMS_URL,API_URL} = CONSTS;
+// const PAGE_A = "http://baseUrl.site/pageA"
+// const PAGE_B = "http://subdomain.baseUrl.site/pageB"
+// const PAGE_C = "http://sudomain2.baseUrl.site/pageC"
 
-const PAGE_A = "http://baseUrl.site/pageA"
-const PAGE_B = "http://subdomain.baseUrl.site/pageB"
-const PAGE_C = "http://sudomain2.baseUrl.site/pageC"
+const PAGE_A = "/404"
+const PAGE_B = "/contact/submit-feedback"
+const PAGE_C = "/contact"
 
-// todo test tabVisit on root
+const PAGE_A_VALIDATOR = ()=>{
+    cy.get('body.404').should('exist')
+    cy.get('h1').invoke('text').should('include','404: Page Not Found')
+}
+const PAGE_B_VALIDATOR = ()=>{
+    cy.get('body.contact-submit-feedback').should('exist')
+    cy.get('.page__header h1').invoke('text').should('include','Submit Feedback')
+}
+const PAGE_C_VALIDATOR = ()=>{
+    cy.get('body.contact').should('exist')
+    cy.get('.page__header h1').invoke('text').should('include','Contact Us')
+}
+
 describe('Cypress Tab Helper',()=>{
     it('names the root window "root" by default',()=>{
-        cy.visit(PAGE_A).then(()=>{
-            cy.debugTabHelper().then(debug=>{
-                expect(debug.myTabNames.length).to.eq(1)
-                expect(debug.myTabNames[0]).to.eq('root')
-            })
+        cy.visit(PAGE_A)
+        PAGE_A_VALIDATOR()
+        //
+        cy.debugTabHelper().then(debug=>{
+            expect(debug.myTabNames.length).to.eq(1)
+            expect(debug.myTabNames[0]).to.eq('root')
         })
-        cy.get('h1').invoke('text').should('include','404: Page Not Found')
     })
     it('accepts a tab_name option when calling cy.visit for tracking the name of the root page',()=>{
-        cy.visit(PAGE_A,{tab_name: 'myRootWindow'}).then(()=>{
-            cy.debugTabHelper().then(debug=>{
-                expect(debug.myTabNames.length).to.eq(1)
-                expect(debug.myTabNames[0]).to.eq('myRootWindow')
-            })
+        cy.visit(PAGE_A,{tab_name: 'myRootWindow'})
+        PAGE_A_VALIDATOR()
+        //
+        cy.debugTabHelper().then(debug=>{
+            expect(debug.myTabNames.length).to.eq(1)
+            expect(debug.myTabNames[0]).to.eq('myRootWindow')
         })
-        cy.get('h1').invoke('text').should('include','404: Page Not Found')
     })
     it('can open a new tab',()=>{
-        cy.openTab(PAGE_B,{tab_name: 'myNewTab'}).then(()=>{
-            cy.get('.page__header h1').invoke('text').should('include','Submit Feedback')
-            cy.debugTabHelper().then(debug=>{
-                expect(debug.myTabNames.length).to.eq(2)
-                expect(debug.myTabNames[0]).to.eq('myRootWindow')
-                expect(debug.myTabNames[1]).to.eq('myNewTab')
-                expect(debug.active_tab_index).to.eq(1)
-            })
+        cy.openTab(PAGE_B,{tab_name: 'myNewTab'})
+        PAGE_B_VALIDATOR()
+        //
+        cy.debugTabHelper().then(debug=>{
+            expect(debug.myTabNames.length).to.eq(2)
+            expect(debug.myTabNames[0]).to.eq('myRootWindow')
+            expect(debug.myTabNames[1]).to.eq('myNewTab')
+            expect(debug.active_tab_index).to.eq(1)
         })
     })
     it('can switch context back to the root window by name',()=>{
         cy.switchToTab('myRootWindow')
+        PAGE_A_VALIDATOR()
+        //
         cy.debugTabHelper().then(debug=>{
             expect(debug.myTabNames.length).to.eq(2)
             expect(debug.myTabNames[0]).to.eq('myRootWindow')
             expect(debug.myTabNames[1]).to.eq('myNewTab')
             expect(debug.active_tab_index).to.eq(0)
         })
-        cy.get('h1').invoke('text').should('include','404: Page Not Found')
     })
     it('can navgiate the popup to another url without switching to it first',()=>{
-        cy.tabVisit(PAGE_C,'myNewTab').then(()=>{
-            cy.debugTabHelper().then(debug=>{
-                expect(debug.myTabNames.length).to.eq(2)
-                expect(debug.myTabNames[0]).to.eq('myRootWindow')
-                expect(debug.myTabNames[1]).to.eq('myNewTab')
-                expect(debug.active_tab_index).to.eq(1)
-            })
-            cy.window().then(win=>{
-                console.log('huh3?',win.ATABNAME)
-            })
-            cy.get('.page__header h1').invoke('text').should('include','Contact Us')
+        cy.tabVisit(PAGE_C,'myNewTab')
+        PAGE_C_VALIDATOR()
+        //
+        cy.debugTabHelper().then(debug=>{
+            expect(debug.myTabNames.length).to.eq(2)
+            expect(debug.myTabNames[0]).to.eq('myRootWindow')
+            expect(debug.myTabNames[1]).to.eq('myNewTab')
+            expect(debug.active_tab_index).to.eq(1)
         })
     })
     it('can open more tabs',()=>{
@@ -65,20 +76,21 @@ describe('Cypress Tab Helper',()=>{
         function additionalTestTab(i){
             return new Cypress.Promise((resolve, reject) =>{
                 let tab_name = `myPopup${i}`;
-                cy.openTab(PAGE_A,{tab_name}).then(()=>{
-                    cy.debugTabHelper().then(debug=>{
-                        namesToCheck.push(tab_name)
-                        expect(debug.myTabs.length).to.eq(2+i+1)
-                        expect(debug.myTabNames.length).to.eq(2+i+1)
-                        expect(debug.myTabNames[0]).to.eq('myRootWindow')
-                        expect(debug.myTabNames[1]).to.eq('myNewTab')
-                        namesToCheck.forEach((name,i2)=>{
-                            console.log('checking',{namesToCheck,b:debug.myTabNames,name,i})
-                            expect(debug.myTabNames[2+i2]).to.eq(name)
-                        })
-                        expect(debug.active_tab_index).to.eq(i+2)
-                        resolve()
+                cy.openTab(PAGE_A,{tab_name})
+                PAGE_A_VALIDATOR()
+                //
+                cy.debugTabHelper().then(debug=>{
+                    namesToCheck.push(tab_name)
+                    expect(debug.myTabs.length).to.eq(2+i+1)
+                    expect(debug.myTabNames.length).to.eq(2+i+1)
+                    expect(debug.myTabNames[0]).to.eq('myRootWindow')
+                    expect(debug.myTabNames[1]).to.eq('myNewTab')
+                    namesToCheck.forEach((name,i2)=>{
+                        //console.log('checking',{namesToCheck,b:debug.myTabNames,name,i})
+                        expect(debug.myTabNames[2+i2]).to.eq(name)
                     })
+                    expect(debug.active_tab_index).to.eq(i+2)
+                    resolve()
                 })
             })
         }
